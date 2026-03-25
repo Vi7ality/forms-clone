b4a17e3c396aa09d68684a724f783553ab73a07d
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-03-25 17:31:48 +0200

    response resolver added

--- server/src/response/response.resolver.ts
+++ server/src/response/response.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { ResponseService } from './response.service';
+import { Response } from 'src/form/models/response.model';
+import { SubmitResponseInput } from './dto/submit-response.input';
 
 @Resolver()
-export class ResponseResolver {}
+export class ResponseResolver {
+  constructor(private responseService: ResponseService) {}
+
+  @Query(() => [Response])
+  responses(@Args('formId') formId: string) {
+    return this.responseService.getResponses(formId);
+  }
+
+  @Mutation(() => Response)
+  submitResponse(@Args('input') input: SubmitResponseInput) {
+    return this.responseService.submitResponse(input);
+  }
+}
