614008a46115f634d253197a9fca395218ac05d2
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-03-25 17:25:21 +0200

    form resolver added

--- server/src/form/form.resolver.ts
+++ server/src/form/form.resolver.ts
@@ -1,4 +1,24 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Form } from './models/form.model';
+import { FormService } from './form.service';
+import { CreateFormInput } from './dto/create-form.input';
 
-@Resolver()
-export class FormResolver {}
+@Resolver(() => Form)
+export class FormResolver {
+  constructor(private formService: FormService) {}
+
+  @Query(() => [Form])
+  forms() {
+    return this.formService.getForms();
+  }
+
+  @Query(() => Form, { nullable: true })
+  form(@Args('id') id: string) {
+    return this.formService.getForm(id);
+  }
+
+  @Mutation(() => Form)
+  createForm(@Args('input') input: CreateFormInput) {
+    return this.formService.createForm(input);
+  }
+}
