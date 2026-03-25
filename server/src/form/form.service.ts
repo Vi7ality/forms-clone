bc8b9753b377a2adc00e61b3be24aca7ac6cdfe1
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-03-25 15:05:25 +0200

    form service added

--- server/src/form/form.service.ts
+++ server/src/form/form.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../common/database.service';
+import { CreateFormInput } from './dto/create-form.input';
+
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class FormService {}
+export class FormService {
+  constructor(private db: DatabaseService) {}
+
+  getForms() {
+    return this.db.forms;
+  }
+
+  getForm(id: string) {
+    return this.db.forms.find((f) => f.id === id);
+  }
+
+  createForm(input: CreateFormInput) {
+    const form = {
+      id: randomUUID(),
+      ...input,
+      questions:
+        input.questions?.map((q) => ({ ...q, id: randomUUID() })) || [],
+    };
+    this.db.forms.push(form);
+    return form;
+  }
+}
