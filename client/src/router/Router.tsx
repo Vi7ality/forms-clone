92494f57a2068a0c5a6f005709f4c16435f69291
Vitalii S. <vitalii.sapunov@valsydev.com>
2026-03-26 16:09:11 +0200

    page added to router

--- client/src/router/Router.tsx
+++ client/src/router/Router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, type RouteObject } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
+import CreateFormPage from "../pages/HomePage/CreateFormPage";
 
 const routes: RouteObject[] = [
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/forms/new",
+    element: <CreateFormPage />,
   },
 ];
 
